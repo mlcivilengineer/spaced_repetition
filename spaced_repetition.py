818b14f6a49c4c7b77065c80819ac5c323ebfe2b
datapylua <luabrudna@gmail.com>
2020-06-01 21:30:20 -0300

    muda os horarios do evento pra ser das 19 as 23:50

--- spaced_repetition.py
+++ spaced_repetition.py
@@ -18,7 +18,7 @@ def create_spaced_repetition_event(service, name, today_date, spaced_time=3, tim
     delta = datetime.timedelta(spaced_time)
     new_date = today_date + delta
     start_date = str(datetime.datetime.combine(date=new_date, time=datetime.time(hour=19)).isoformat("T"))
-    end_date = str(datetime.datetime.combine(date=new_date, time=datetime.time(hour=20)).isoformat("T"))
+    end_date = str(datetime.datetime.combine(date=new_date, time=datetime.time(hour=23,minute=50)).isoformat("T"))
     event = {
         'summary': name,
         'start': {
@@ -49,32 +49,35 @@ def create_spaced_repetition_event(service, name, today_date, spaced_time=3, tim
 # with open('token.pickle', 'wb') as token:
 #     pickle.dump(creds, token)
 
+def main(*args):
+    with open('credentials/token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+    service = build('calendar', 'v3', credentials=creds)
 
-with open('credentials/token.pickle', 'rb') as token:
-    creds = pickle.load(token)
-service = build('calendar', 'v3', credentials=creds)
+    today_date = datetime.date.today()
+    NOTION_URL = 'https://www.notion.so/Study-Notes-be00b11bb10844a7aee3ce59f3454bd2'
 
-today_date = datetime.date.today()
-NOTION_URL = 'https://www.notion.so/Study-Notes-be00b11bb10844a7aee3ce59f3454bd2'
+    DRIVER_PATH = './drivers/chromedriver'
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
 
-DRIVER_PATH = './drivers/chromedriver'
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(DRIVER_PATH, options=chrome_options)
+    driver.get(NOTION_URL)
 
-driver = webdriver.Chrome(DRIVER_PATH, options=chrome_options)
-driver.get(NOTION_URL)
+    response = wait_and_get_element(
+        '#notion-app > div > div.notion-cursor-listener > div.notion-frame > div.notion-scroller.vertical.horizontal > div.notion-page-content',
+        driver=driver)
 
-response = wait_and_get_element(
-    '#notion-app > div > div.notion-cursor-listener > div.notion-frame > div.notion-scroller.vertical.horizontal > div.notion-page-content',
-    driver=driver)
+    print(response.text)
 
-print(response.text)
+    if '@Today' in response.text:
+        title = f'Study Recap of {str(today_date)}'
+        print(f'creating events for {title}')
+        create_spaced_repetition_event(service, title, today_date, spaced_time=1)
+        create_spaced_repetition_event(service, title, today_date, spaced_time=4)
+        create_spaced_repetition_event(service, title, today_date, spaced_time=11)
+        create_spaced_repetition_event(service, title, today_date, spaced_time=32)
 
-if '@Today' in response.text:
-    title = f'Study Recap of {str(today_date)}'
-    print(f'creating events for {title}')
-    create_spaced_repetition_event(service, title, today_date, spaced_time=1)
-    create_spaced_repetition_event(service, title, today_date, spaced_time=4)
-    create_spaced_repetition_event(service, title, today_date, spaced_time=11)
-    create_spaced_repetition_event(service, title, today_date, spaced_time=32)
+if __name__ == '__main__':
+    main()
